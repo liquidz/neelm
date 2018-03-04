d5a14c2d1c5ec784bedc5870324d4ebcf6252b4f
liquidz <liquidz.uo@gmail.com>
2018-03-04 04:58:36 +0000

    Add several operation functions

--- src/neelm/operation.clj
+++ src/neelm/operation.clj
@@ -1,10 +1,11 @@
 (ns neelm.operation
   "Matrix Operation"
-  (:require [uncomplicate.neanderthal.core :refer :all]
+  (:refer-clojure :exclude [shuffle partition])
+  (:require [neelm.operation :as op]
+            [uncomplicate.neanderthal.core :refer :all]
             [uncomplicate.neanderthal.linalg :as n.l]
             [uncomplicate.neanderthal.native :refer :all])
-  (:import uncomplicate.neanderthal.internal.api.RealMatrix
-           uncomplicate.neanderthal.internal.api.RealVector))
+  (:import [uncomplicate.neanderthal.internal.api RealMatrix RealVector]))
 
 (defn- randoms []
   (let [r (java.util.Random. (System/currentTimeMillis))]
@@ -55,8 +56,19 @@
     (divide! a' b)
     a'))
 
+(defmulti ensure-column-layout (comp :layout info))
+(defmethod ensure-column-layout :column
+  [mat] mat)
+(defmethod ensure-column-layout :row
+  [mat]
+  (let [[m n] (shape mat)
+        res (dge m n)]
+    (copy! mat res)
+    res))
+
 ;; https://software.intel.com/en-us/articles/implement-pseudoinverse-of-a-matrix-by-intel-mkl
-(defn pinv [mat & {:keys [alpha beta] :or {alpha 1.0 beta 0.0}}]
+(defn pinv [mat]
+  {:pre [(= :column (:layout (info mat)))]}
   (let [[m n] (shape mat)
         k (min m n)
         {:keys [sigma u vt]} (n.l/svd mat true true)
@@ -65,7 +77,7 @@
       (let [si (entry sigma i i)
             ss (double (if (> si 1.0e-9) (/ 1.0 si) si))]
         (scal! ss (col u i))))
-    (mm! alpha (trans vt) (trans u) beta inva)
+    (mm! 1.0 (trans vt) (trans u) inva)
     inva))
 
 (defn ensure-matrix [x]
@@ -96,10 +108,82 @@
 (defn inv [mat]
   (-> mat n.l/trf n.l/tri))
 
-(defn identity-matrix
-  ([n] (identity-matrix n 1.0))
+(defn diagonal-matrix
+  ([n] (diagonal-matrix n 1.0))
   ([n val]
    (let [mat (dge n n)]
      (dotimes [i n]
        (entry! mat i i val))
      mat)))
+
+(defn shuffle
+  [& matrixes]
+  (let [m (-> matrixes first mrows)
+        indexes (-> m range clojure.core/shuffle)]
+    (for [mat matrixes
+          :let [res (dge (mrows mat) (ncols mat))]]
+      (do (dotimes [i m]
+            (copy! (row mat (nth indexes i))
+                   (row res i)))
+          res))))
+
+(defn partition
+  ([n mat] (partition n mat false))
+  ([n mat copy?]
+   (let [[num-row num-col] (op/shape mat)]
+     (for [grp (clojure.core/partition n (range num-row))]
+       (cond-> (submatrix mat (first grp) 0
+                          n num-col)
+         copy? copy)))))
+
+(defn concat-cols
+  "each matrixes must have same mrows"
+  [& matrixes]
+  (let [num-row (-> matrixes first mrows)
+        num-cols (map ncols matrixes)
+        res (dge num-row (reduce + num-cols)) ]
+    (loop [[mat & rest-matrixes] matrixes
+           [num-col & rest-num-cols] num-cols
+           col-start 0]
+      (when mat
+        (copy! mat (submatrix res 0 col-start num-row num-col))
+        (recur rest-matrixes rest-num-cols (+ col-start num-col))))
+    res))
+
+(defn concat-rows
+  "each matrixes must have same ncols"
+  [& matrixes]
+  (let [num-rows (map mrows matrixes)
+        num-col (-> matrixes first ncols)
+        res (dge (reduce + num-rows) num-col) ]
+    (loop [[mat & rest-matrixes] matrixes
+           [num-row & rest-num-rows] num-rows
+           row-start 0]
+      (when mat
+        (copy! mat (submatrix res row-start 0 num-row num-col))
+        (recur rest-matrixes rest-num-rows (+ row-start num-row))))
+    res))
+
+(defn safe-trans
+  "transpose matrix without changing matrix layout"
+  [mat]
+  (ensure-column-layout (trans mat)))
+
+(defn- vmax [v] (entry v (imax v)))
+(defn- vmin [v] (entry v (imin v)))
+
+(defn normalize
+  ([x] (normalize x 0.0 1.0))
+  ([x ymin ymax]
+   (let [[num-row num-col] (shape x)
+         n (* num-row num-col)
+         xs (flatten (seq x))
+         xmax (reduce max xs)
+         xmin (reduce min xs)
+         x' (copy x)]
+     (alter! x' (fn ^double [^long i ^long j ^double xi]
+                  (+ (* (/ (- xi xmin)
+                           (- xmax xmin))
+                        (- ymax ymin))
+                     ymin)))
+     x')))
