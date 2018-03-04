0d78c4c52730234a6f6755eee4e9a5eb6a82cb28
liquidz <liquidz.uo@gmail.com>
2018-03-04 04:59:56 +0000

    Update to use operation/normalize

--- src/neelm/util.clj
+++ src/neelm/util.clj
@@ -1,28 +1,19 @@
 (ns neelm.util
+  "Utilities for dataset"
   (:require [uncomplicate.neanderthal.core :refer :all]
             [uncomplicate.neanderthal.native :refer :all]
             [neelm.operation :as op]))
 
-(defn- vmax [v]
-  (entry v (imax v)))
+(defn normalize
+  "Normalize inputed sequence
 
-(defn- vmin [v]
-  (entry v (imin v)))
-
-(defn- normalize* [x from-val to-val]
-  (let [cs (cols x)
-        n (-> cs first dim)
-        cols-max (dv (map vmax cs))
-        cols-min (dv (map vmin cs))
-        std (op/divide (op/minus x cols-min)
-                       (op/minus cols-max cols-min))]
-    (op/plus (scal (- to-val from-val) std)
-             (dv (repeat (ncols x) from-val)))))
-
-(defn normalize [x & [opt]]
+  * Defaults
+    * opt `{:from 0.0 :to 1.0}`
+  "
+  [x & [opt]]
   (let [from (:from opt 0.0)
         to (:to opt 1.0)
         mrows (count x)
         ncols (-> x first count)
-        mat (dge mrows ncols (flatten x) {:layout :row})]
-    (seq (normalize* mat from to))))
+        mat (op/ensure-matrix x)]
+    (apply map list (op/normalize mat from to))))
