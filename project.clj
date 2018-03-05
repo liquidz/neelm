b2965016cf0fa33f95ecad5b1a315c8ff1bb6f44
liquidz <liquidz.uo@gmail.com>
2018-03-05 18:21:12 +0900

    Update library dependencies

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.taoensso/nippy "2.13.0"]
+                 [com.taoensso/nippy "2.14.0"]
                  [uncomplicate/neanderthal "0.18.0"]]
   :profiles
   {:dev {:source-paths  ["example"]
          :dependencies [[fudje  "0.9.7"]
-                        [com.taoensso/tufte  "1.1.2"]
-                        [nz.ac.waikato.cms.weka/weka-stable "3.8.0"]
-                        [incanter/incanter-core "1.5.7"]
-                        [incanter/incanter-charts "1.5.7"]]
+                        [com.taoensso/tufte  "1.1.3"]
+                        [nz.ac.waikato.cms.weka/weka-stable "3.8.2"]
+                        [incanter/incanter-core "1.9.2"]
+                        [incanter/incanter-charts "1.9.2"]]
          :plugins [[lein-codox "0.10.3"]]
          :repl-options {:timeout 3600000}
          :jvm-opts  ["-Xmx1000m"]}}
